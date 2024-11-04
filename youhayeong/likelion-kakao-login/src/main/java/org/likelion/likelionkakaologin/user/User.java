7fd04d4a03f0f5467d5fb1f6261e41aed5c7a06d
ttttkii913 <dbgkdud821@daum.net>
2024-11-04 13:49:23 +0900

    15주차 과제

---
+++ youhayeong/likelion-kakao-login/src/main/java/org/likelion/likelionkakaologin/user/User.java
@@ -0,0 +1,32 @@
+package org.likelion.likelionkakaologin.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    private Long id;
+
+    @Column(name = "USER_NAME", nullable = false)
+    private String name;
+
+    @Column(name = "USER_EMAIL", nullable = false)
+    private String email;
+
+    @Column(name = "USER_PICTURE_URL")
+    private String pictureUrl;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "USER_ROLE", nullable = false)
+    private Role role;
+}
